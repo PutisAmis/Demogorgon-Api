6f7281c35a14196f364b8f230dd4f429acd9776b
Ismael Jimenez Lorente <ismael.jimenez.lorente@gmail.com>
2017-04-09 18:11:06 +0200

    user model and location

--- models/user.js
+++ models/user.js
@@ -2,14 +2,60 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
-    name: String,
-    provider: String,
-    provider_id: String,
-    email: String,
-    picture: String,
-    role: {type: String, enum:["superAdministrator", "administrator", 'inCharge', 'basic']},
-    signedUpDate: {type: Date, default: Date.now()},
-    lastDate: Date
+    name: {
+        type: String,
+        required: true
+    },
+    provider: {
+        type: String,
+        enum: ["facebook", "google", "tattoo"],
+        required: true
+    },
+    provider_id: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    picture: {
+        type: String,
+        default: 'default.png',
+        required: true
+    },
+    role: {
+        type: String, 
+        required: true, 
+        enum:["sa", "administrator", 'basic', 'tattoo_artist', 'customer']
+    },
+    signedUpDate: {
+        type: Date, 
+        default: Date.now(),
+        required: true
+    },
+    lastDate: {
+        type: Date,
+        required: true
+    },
+    loc: {
+        type:{
+            type: String,
+            required: true,
+            enum: ['Point', 'LineString', 'Polygon'],
+            default: 'Point'
+        },
+        coordinates: [Number]
+    },
+    loc_name: {
+        type:String,
+        required: true
+    },
+    sanitary: {
+        type: Boolean,
+        default: false,
+        required: true
+    }
 });
 
 const userModel = mongoose.model("User", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ require("../lib/passportfacebook");
 
 module.exports = app => {
 
-    app.get('/auth/facebook', passport.authenticate('facebook', { session: false, scope: ['public_profile', 'email'] }));
+    app.get('/auth/facebook', passport.authenticate('facebook', { session: false, scope: ['public_profile', 'email', 'user_location'] }));
 
     app.get('/auth/facebook/callback', passport.authenticate('facebook', { session: false }), (req, res)=>{
         let response = req.user === null? {token: null, error: true} : {token: req.user, error: false};
