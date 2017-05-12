10c5dc41215ea77d5c096be59330d6eb21504317
Ismael Jimenez Lorente <ismael.jimenez.lorente@gmail.com>
2017-05-12 21:16:18 +0200

    Refactoring auth

--- routes/auth.js
+++ routes/auth.js
@@ -7,14 +7,14 @@ module.exports = app => {
     app.get('/auth/facebook', passport.authenticate('facebook', { session: false, scope: ['public_profile', 'email', 'user_location'] }));
 
     app.get('/auth/facebook/callback', passport.authenticate('facebook', { session: false }), (req, res)=>{
-        let response = req.user === null? {token: null, error: true} : {token: req.user, error: false};
+        let response = res.error? res.error : res.user;
         res.send(response);
     });
 
     app.get('/auth/google', passport.authenticate('google', { session: false, scope : ['profile', 'email'] }));
 
     app.get('/auth/google/callback', passport.authenticate('google', { session: false }), (req, res)=>{
-        let response = req.user === null? {token: null, error: true} : {token: req.user, error: false};
+        let response = res.error? res.error : res.user;
         res.send(response);
     });
 }
